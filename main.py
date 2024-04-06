48cb1b7e84275f9919ac3f8e58d82af5641a17ed
Retr0991 <112775305+Retr0991@users.noreply.github.com>
2024-04-07 04:59:55 +0530

    added mongoDB connection

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+password = os.getenv("PASS")
 
 app = FastAPI()
 
 # Connect to MongoDB
-client = MongoClient('mongodb://localhost:27017/')
-db = client['test']
+client = MongoClient(f'mongodb+srv://retr0991:{password}@cluster0.rtkjeyb.mongodb.net/')
+# client = MongoClient('mongodb://localhost:27017/')
+db = client['clinicalsentix']
 collection = db['test']
 
 # Endpoint to get all items
 @app.get("/items/")
 async def read_items():
-    items = list(collection.find())
+    items = list(collection.find_one({}, {"_id": 0}))  # Exclude _id field from response
     return items
 
 # Endpoint to get a single item by ID
-@app.get("/items/{item_id}")    
-async def read_item(item_id: str):
-    item = collection.find_one({"_id": item_id})
-    if item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return item
-
-# Endpoint to update an item
-@app.put("/items/{item_id}")
-async def update_item(item_id: str, new_item: dict):
-    updated_item = collection.find_one_and_update(
-        {"_id": item_id}, {"$set": new_item}, return_document=True
-    )
-    if updated_item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return updated_item
+@app.get("/items/{item_name}")    
+async def read_item(item_name: str):
+    drugName = item_name
+    items = collection.find_one({}, {"_id": 0})
+    try:
+        # Dict
+        item = items[drugName]
+        new_data = {item_name: {"topic" : [], "tweets": []}}
+        for key, value in item.items():
+            name = value["name"]
+            count = value["count"]
+            tweet = list(value["tweet"].values())
+            if(key != "topic_number_-1"):
+                new_data[item_name]["topic"].append({"name": name, "count": count})
+                new_data[item_name]["tweets"].append({"topic_name": name, "tweets": tweet})
+    except KeyError:
+        raise HTTPException(status_code=404, detail=f"{drugName} not found")
+    return new_data
